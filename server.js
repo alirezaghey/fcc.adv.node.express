62c35987cf2a4f0f7448b5a4efc5ebc56c6bb528
alirezaghey <26653424+alirezaghey@users.noreply.github.com>
2021-06-11 23:55:56 +0200

    solved 3rd exercise

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 "use strict";
 require("dotenv").config();
 const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
 const path = require("path");
 const myDB = require("./connection");
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
@@ -12,6 +14,16 @@ app.set("view engine", "pug");
 app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.route("/").get((req, res) => {
   res.render(path.join(__dirname, "views/pug"), {
