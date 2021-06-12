f2765de31848bf3621f6faa80e25fc23194cbdc6
alirezaghey <26653424+alirezaghey@users.noreply.github.com>
2021-06-12 19:54:24 +0200

    solved 6th exercise

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const session = require("express-session");
 const passport = require("passport");
+const LocalStrategy = require("passport-local");
 const path = require("path");
 const { ObjectID } = require("mongodb");
 const myDB = require("./connection");
@@ -28,6 +29,17 @@ app.use(passport.session());
 
 myDB(async (client) => {
   const myDataBase = await client.db("database").collection("users");
+  passport.use(
+    new LocalStrategy((username, password, done) => {
+      myDataBase.findOne({ username: username }, (err, user) => {
+        console.log(`User ${username} attempted to log in.`);
+        if (err) return done(err);
+        if (!user) return done(null, false);
+        if (password !== user.password) return done(null, false);
+        return done(null, user);
+      });
+    })
+  );
   app.route("/").get((req, res) => {
     res.render(path.join(__dirname, "views/pug"), {
       title: "Connected to Database",
