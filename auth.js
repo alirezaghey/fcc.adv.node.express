b22d7f6a36ab612cc44d5fbdac6d7774b888cccb
alirezaghey <26653424+alirezaghey@users.noreply.github.com>
2021-06-13 20:57:25 +0200

    solved 15th exercise

--- auth.js
+++ auth.js
@@ -2,6 +2,8 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const bcrypt = require("bcrypt");
 const { ObjectID } = require("mongodb");
+const GithubStrategy = require("passport-github").Strategy;
+require("dotenv").config();
 
 module.exports = function (app, myDataBase) {
   passport.use(
@@ -15,6 +17,19 @@ module.exports = function (app, myDataBase) {
         return done(null, user);
       });
     })
+  );
+  passport.use(
+    new GithubStrategy(
+      {
+        clientID: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        callbackURL:
+          "https://fccadvnodeexpress.alirezaghey.repl.co/auth/github/callback",
+      },
+      (accessToken, refreshToken, profile, cb) => {
+        console.log(profile);
+      }
+    )
   );
   passport.serializeUser((user, done) => {
     done(null, user._id);
